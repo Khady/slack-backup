6d2350582b94a00ede34da61ed4cd0d2e918709d
Louis Roché <louis@louisroche.net>
2016-04-28 22:42:16 +0200

    Remove unecessary catching of slacko exceptions

--- lib/slack_backup_lib.ml
+++ lib/slack_backup_lib.ml
@@ -9,33 +9,30 @@ let error_to_string = function
   | `Is_archived -> "Channel is archived"
   | `Msg_too_long -> "Message too long"
   | `Not_authed -> "Not_authed"
+  | `ParseFailure e -> Printf.sprintf "Parse failure: %s" e
   | `Rate_limited -> "Rate limit active"
   | `Success _ -> "Success"
   | `Unhandled_error e -> Printf.sprintf "Unhandled error: %s" e
   | `Unknown_error -> "Unknown error"
+  | `User_is_bot -> "User is a bot"
   | `User_not_found -> "User is not found"
   | `User_not_visible -> "User is not visible"
 
 let fetch_history fetch =
   let open Yojson.Basic.Util in
-  let to_json_list j = `List j in
+  let open Slacko in
   let rec fetch_all ?latest acc =
     match%lwt fetch latest with
-    | `Success json ->
-      let has_more = json |> member "has_more" |> to_bool in
-      let messages = json |> member "messages" |> to_list |> List.rev in
-      if has_more then
-        match messages with
+    | `Success history_obj ->
+      if history_obj.has_more then
+        match history_obj.messages with
         | [] ->
-          Lwt.return @@ Result.Ok (to_json_list acc)
+          Lwt.return @@ Result.Ok acc
         | m::_ ->
-          let latest_ts = m |> member "ts" |> to_string |> float_of_string in
-          fetch_all ~latest:latest_ts (messages @ acc)
+          fetch_all ~latest:m.ts (history_obj.messages @ acc)
       else
-        Lwt.return @@ Result.Ok (to_json_list @@ messages @ acc)
+        Lwt.return @@ Result.Ok (history_obj.messages @ acc)
     | e -> Lwt.return @@ Result.Error e
-    | exception e ->
-      Lwt.return @@ Result.Error (`Unhandled_error (Printexc.to_string e))
   in
   fetch_all []
 
@@ -61,41 +58,23 @@ let fetch_channel ?latest ?oldest ?count token channel =
        Slacko.channels_history ?latest ?oldest ?count token channel
     )
 
-let string_id_of_user token user =
-  let open Yojson.Basic.Util in
-  try%lwt
-    Slacko.users_info token user >|= function
-    | `Success json ->
-      let id = json |> member "user" |> member "id" |> to_string in
-      Result.Ok id
-    | e -> Result.Error e
-  with e ->
-    (* TODO: Treat Slacko.No_matches as a specific case *)
-    Lwt.return @@ Result.Error (`Unhandled_error (Printexc.to_string e))
-
 let conversation_of_user token user =
   let open Yojson.Basic.Util in
-  match%lwt string_id_of_user token user with
-  | (Result.Error _) as e -> Lwt.return e
-  | Result.Ok user_id ->
-    Slacko.im_list token >|= function
-    | `Success json ->
-      let conversations = json |> member "ims" |> to_list in
-      begin
-        try
-          Result.Ok (
-            List.find (fun disc_json ->
-                let u = disc_json |> member "user" |> to_string in
-                user_id = u
-              ) conversations
-            |> member "id"
-            |> to_string
-            |> Slacko.conversation_of_string
-          )
-        with Not_found ->
-          Result.Error `Channel_not_found
-      end
-    | e -> Result.Error e
+  let open Slacko in
+  im_list token >|= function
+  | `Success im_objs ->
+    begin
+      try
+        let im =
+          List.find (fun (im_obj : im_obj) ->
+              im_obj.user = user
+            ) im_objs
+        in
+        Result.Ok (conversation_of_string im.id)
+      with Not_found ->
+        Result.Error `Unknown_error
+    end
+  | e -> Result.Error e
 
 let verify_token token =
   match%lwt Slacko.auth_test token with
@@ -103,5 +82,3 @@ let verify_token token =
     Lwt.return @@ Result.Ok ()
   | e ->
     Lwt.return @@ Result.Error e
-  | exception e ->
-    Lwt.return @@ Result.Error (`Unhandled_error (Printexc.to_string e))
--- lib/slack_backup_lib.mli
+++ lib/slack_backup_lib.mli
@@ -7,19 +7,20 @@ val error_to_string :
   | `Is_archived
   | `Msg_too_long
   | `Not_authed
+  | `ParseFailure of string
   | `Rate_limited
   | `Success of 'a
   | `Unhandled_error of string
   | `Unknown_error
+  | `User_is_bot
   | `User_not_found
-  | `User_not_visible
-  ] ->
+  | `User_not_visible ] ->
   string
 (** Convert a slacko error to a string. *)
 
 val fetch_history :
   (float option -> Slacko.history_result Lwt.t) ->
-  (Yojson.Basic.json, Slacko.history_result) Result.result Lwt.t
+  (Slacko.message_obj list, Slacko.history_result) Result.result Lwt.t
 
 val fetch_conversation :
   ?latest:Slacko.timestamp ->
@@ -27,7 +28,7 @@ val fetch_conversation :
   ?count:int ->
   Slacko.token ->
   Slacko.conversation ->
-  (Yojson.Basic.json, Slacko.history_result) Result.result Lwt.t
+  (Slacko.message_obj list, Slacko.history_result) Result.result Lwt.t
 
 val fetch_channel :
   ?latest:Slacko.timestamp ->
@@ -35,26 +36,22 @@ val fetch_channel :
   ?count:int ->
   Slacko.token ->
   Slacko.channel ->
-  (Yojson.Basic.json, Slacko.history_result) Result.result Lwt.t
-
-val string_id_of_user :
-  Slacko.token ->
-  Slacko.user ->
-  (string,
-   [> Slacko.authed_result | Slacko.user_error | Slacko.user_visibility_error ]
-  ) Result.result Lwt.t
+  (Slacko.message_obj list, Slacko.history_result) Result.result Lwt.t
 
 val conversation_of_user :
   Slacko.token ->
   Slacko.user ->
   (Slacko.conversation,
-   [> Slacko.authed_result
-   | Slacko.user_error
-   | Slacko.user_visibility_error
-   | Slacko.channel_error
+   [ Slacko.parsed_auth_error
+   | Slacko.bot_error
+   | `Success of Slacko.im_obj list
    ]
   ) Result.result Lwt.t
 
 val verify_token :
   Slacko.token ->
-  (unit, [> Slacko.authed_result ]) Result.result Lwt.t
+  (unit,
+   [ `Success of Slacko.authed_obj
+   | Slacko.parsed_auth_error
+   ]
+  ) Result.result Lwt.t
